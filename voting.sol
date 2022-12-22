ac6714734c97d8c08831e1a015f9d580708541a4
Luka <Lazarashvili02@gmail.com>
2022-12-23 01:16:22 +0400

    add structs

--- voting.sol
+++ voting.sol
@@ -2,4 +2,23 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Voting {}
+contract Voting {
+    struct Voter {
+        address addr;
+        string fullname;
+        uint256 identicalNumber;
+    }
+
+    mapping(address => Voter) voters;
+}
+
+contract Candidates {
+    struct Candidate {
+        uint8 participantNumber;
+        address addr;
+        string fullname;
+        uint256 identicalNumber;
+    }
+
+    mapping(address => Candidate) candidates;
+}
