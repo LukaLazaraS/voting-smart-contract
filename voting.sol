15676d645c61930a1985eb9e461f034109ec0f70
LukaLazarashvili <Lazarashvili02@gmail.com>
2022-12-24 04:45:49 +0400

    update candidates and voting contracts

--- voting.sol
+++ voting.sol
@@ -3,22 +3,104 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Voting {
+    mapping(address => Voter) voters;
+    Voter[] votersArr;
+    address private owner;
+
     struct Voter {
-        address addr;
         string fullname;
-        uint256 identicalNumber;
+        string identicalNumber;
+        uint8 age;
     }
 
-    mapping(address => Voter) voters;
+    modifier isOwner() {
+        require(msg.sender == owner, "You must be the Owner");
+        _;
+    }
+
+    modifier is18(uint8 _age) {
+        require(_age > 17, "The minimum age for voting is 18");
+        _;
+    }
+
+    modifier isZeroBalance() {
+        require(
+            msg.sender.balance > 100000000000000,
+            "You must have more than 0.0001 ether"
+        );
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function viewAllVoters() public view returns (uint, Voter[] memory) {
+        return (votersArr.length, votersArr);
+    }
+
+    function registerAsVoter(
+        string calldata _fullname,
+        string calldata _identicalNumber,
+        uint8 _age
+    ) external is18(_age) isZeroBalance {
+        voters[msg.sender] = Voter(_fullname, _identicalNumber, _age);
+        votersArr.push(Voter(_fullname, _identicalNumber, _age));
+    }
+
+    fallback() external payable {}
+
+    receive() external payable {}
 }
 
 contract Candidates {
+    mapping(uint8 => Candidate) candidates;
+    Candidate[] candidatesArr;
+    uint8 maxCandidates = 10;
+    address owner;
+
     struct Candidate {
         uint8 participantNumber;
-        address addr;
-        string fullname;
         uint256 identicalNumber;
+        string fullname;
+        string slogan;
+        uint256 votes;
     }
 
-    mapping(address => Candidate) candidates;
+    modifier isOwner() {
+        require(msg.sender == owner, "You must be the Owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addCandidate(
+        uint8 _participantNumber,
+        uint256 _identicalNumber,
+        string calldata _fullname,
+        string calldata _slogan
+    ) external isOwner {
+        candidates[_participantNumber] = Candidate(
+            _participantNumber,
+            _identicalNumber,
+            _fullname,
+            _slogan,
+            0
+        );
+        candidatesArr.push(
+            Candidate(
+                _participantNumber,
+                _identicalNumber,
+                _fullname,
+                _slogan,
+                0
+            )
+        );
+    }
+
+    fallback() external payable {}
+
+    receive() external payable {}
 }
